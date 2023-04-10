80c7458e0554548e203d70d3fa2fe1ff3b6b0bec
Albin Lindell <albinglindell98@gmail.com>
2023-04-11 00:01:18 +0200

    first init

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import boxBottom from "./assets/box.png"
+import boxBackground from "./assets/backgroundBox.png"
+import boxTop from "./assets/boxtop.png"
+import adrian from "./assets/flygplan.png"
+import earth from "./assets/earth.png"
+import './App.css'
+
+function App() {
+  const [open, setOpen] = useState(false)
+  const openGift = ()=>{
+    setOpen(!open)
+  }
+  return (
+    <div className="App">
+      <div className="container">
+      <div className={!open ? "gift" : "gift open"}>
+       <img className={!open ? "adrian" : "adrian open"} src={adrian} alt="adrian och jag" />
+        <img className='earth' src={earth} alt="jorden" />
+      </div>
+      <div className={!open ? "box" : "box open"}>
+        <img className={!open ?"boxTop":"boxTop open"} src={boxTop} alt="boxtop" />
+        <img className='boxBackground' src={boxBackground} alt="boxBackground" />
+        <img className='boxBottom' src={boxBottom} alt="box" />
+    </div>
+      </div>
+     <button onClick={openGift}>Öppna present!</button>
+     <h3 className={!open ? "" : "open"}>Du får en valfri weekend med mig i höst!</h3>
+    </div>
+  )
+}
+
+export default App
